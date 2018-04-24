e39dd3486e1dc41f9390f751e8864e0337cc746d
Tomas Lin <horsekit1982@gmail.com>
2018-04-25 00:23:31 +0800

    新增 task 刪除 task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import moment from 'moment';
+import { Segment, Header, Input, List, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [],
+      isFetching: false,
+      task: ''
+    }
+  }
+
+  createTask(text){
+    return {
+      text,
+      created_at: moment().format('YYYY MM DD hh:mm:ss')
+    }
+  }
+
+  addTask = (text) => {
+    const task = this.createTask(text);
+    const {todos} = this.state;
+    todos.push(task);
+    this.setState({todos});
+  }
+
+  deleteTask = (index) => {
+    const {todos} = this.state;
+    todos.splice(index, 1);
+    this.setState({todos});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Segment>
+          <Header>Todo Demo</Header>
+          <Input onChange={(e, {value}) => this.setState({task: value})} value={this.state.task}/> <Button icon='plus'  onClick={(e, {value}) => this.addTask(value)} value={this.state.task}></Button>
+          <Segment basic>
+          <List divided relaxed>
+            {this.state.todos.map((todo, index) => {
+              return (
+                <List.Item key={`todo-${index}`}>
+                  <List.Icon name='trash' color='red' size='large' verticalAlign='middle' onClick={() => this.deleteTask(index)}/>
+                  <List.Content>
+                    <List.Header as='a'>{todo.text}</List.Header>
+                    <List.Description as='a'>{todo.created_at}</List.Description>
+                  </List.Content>
+                </List.Item>
+              );
+            })}
+            
+          </List>
+          </Segment>
+        </Segment>
     );
   }
 }
